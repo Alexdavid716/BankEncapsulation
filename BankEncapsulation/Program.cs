11bd5806524b9c445b7704479b253855a231235e
Alex David <alexdavid716@gmail.com>
2024-03-04 09:34:08 -0500

    finished

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -5,10 +5,10 @@
         static void Main(string[] args)
         {
             {
-                
+
                 BankAccount account = new BankAccount();
 
-               
+
                 bool exit = false;
                 while (!exit)
                 {
@@ -18,10 +18,10 @@
                     Console.WriteLine("3. Exit");
                     Console.Write("Enter your choice: ");
 
-                    
+
                     string choice = Console.ReadLine();
 
-                    
+
                     switch (choice)
                     {
                         case "1":
@@ -51,27 +51,14 @@
                             break;
                     }
 
-                    Console.WriteLine(); 
+                    Console.WriteLine();
                 }
 
                 Console.WriteLine("Thank you for banking with us!");
             }
         }
 
-        public class BankAccount
-        {
-            private double balance = 0;
 
-            public void Deposit(double amount)
-            {
-                balance += amount;
-            }
-
-            public double GetBalance()
-            {
-                return balance;
-            }
-        }
     }
-    }
+}
 
